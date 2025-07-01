6bbb6e2809595b3c7651ddc9f29e53b6a01630a4
mightbegood12 <banunaresh1976@gmail.com>
2025-07-01 17:14:37 +0530

    [add] functions

--- functions.js
+++ functions.js
@@ -1,11 +1,23 @@
 names = ["Raja", "Damo", "Ibrahim", "Selva", "Ashwin"]
+salaries = [20000, 40000, 50000, 20000, 44000, 20000]
 
-nums = [20, 40, 50, 20, 44]
 
 // adding elements
-names.push("Joseph")
-console.log(names)
+names.push("Joseph");
+// console.log(names);
 
-    
+// sorting elements
+names.sort();
+// console.log(names);
 
+// looping throught the names list
+names.forEach(name => {
+    // console.log("Hi, " + name)
+});
 
+const dict = {};
+for (let size = 0; size < names.length; size++) {
+  dict[names[size]] = salaries[size]
+}
+
+console.log(dict)
